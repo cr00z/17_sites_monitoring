402aa760a85d2778f4a713c7f4972ef5c8ea45d5
cr00z <netrebinr@gmail.com>
2019-04-16 19:41:16 +0700

    refactor is_domain_expired func

--- check_sites_health.py
+++ check_sites_health.py
@@ -42,40 +42,42 @@ def get_domain_expiration_date(domain_name):
     return domain_info.expiration_date
 
 
-def get_domains_info(urls4check):
+def get_domains_info(urls4check, limit):
     return [
         {
             'url': checked_url,
             'status': is_server_respond_with_ok(checked_url),
-            'expdate': get_domain_expiration_date(checked_url)
+            'expired': is_domain_expired(
+                get_domain_expiration_date(checked_url),
+                limit
+            )
         }
         for checked_url in urls4check]
 
 
 def is_domain_expired(expdate, limit):
     if isinstance(expdate, list):
-        for expdate_candidate in expdate:
-            if isinstance(expdate_candidate, datetime.datetime):
-                expdate = expdate_candidate
-                break
+        try:
+            expdate = min(expdate)
+        except TypeError:
+            return None
     if isinstance(expdate, datetime.datetime):
         days_remaining = (expdate - datetime.datetime.now()).days
         if days_remaining is not None:
-            return True if days_remaining <= limit else False
+            return days_remaining <= limit
 
 
-def print_domains_info(domains_info, limit):
+def print_domains_info(domains_info):
     url_max_len = len(max(
         [domain_info['url'] for domain_info in domains_info],
         key=len
     ))
     print('{} STATUS EXPIRE'.format('URL'.ljust(url_max_len)))
     for domain_info in domains_info:
-        domain_expired = is_domain_expired(domain_info['expdate'], limit)
         print('{} {} {}'.format(
             domain_info['url'].ljust(url_max_len),
             'ok    ' if domain_info['status'] else 'error ',
-            'unknown' if domain_expired is None else 'yes' if domain_expired else 'no'
+            'unknown' if domain_info['expired'] is None else 'yes' if domain_info['expired'] else 'no'
         ))
 
 
@@ -84,5 +86,5 @@ if __name__ == '__main__':
     urls4check = load_urls4check(args.urls_file_path)
     if not urls4check:
         exit('incorrect file with urls')
-    domains_info = get_domains_info(urls4check)
-    print_domains_info(domains_info, args.limit)
+    domains_info = get_domains_info(urls4check, args.limit)
+    print_domains_info(domains_info)
